dc982f12727f0d244010a29dd1b3d42503d6e4f5
Mayuran-Sundermurthy <87370153+Mayuran-Sundermurthy@users.noreply.github.com>
2021-08-19 20:57:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function preload(){
 }
 
 function setup() {
-    createCanvas(1920,500);
+    createCanvas(windowWidth,500);
     player = createSprite(50,height/2,35,35);
     player.shapeColor = 'green';
     invisible1 = createSprite(width/2,0,width,10);
@@ -48,20 +48,14 @@ function draw() {
         player.collide(invisible1);
         player.collide(invisible2);
         createEdgeSprites();
-        
-        
-    
-    
-        
-    
-    
+       
     drawSprites();
     text("Score: "+ score, 50, 20);
 }
 
 function createWalls(){
     if(frameCount % 30 == 0){
-        obstacle = createSprite(2000,Math.round(random(35,465)), 10, Math.round(random(10,120)));
+        obstacle = createSprite(width+10,Math.round(random(35,465)), 10, Math.round(random(10,120)));
         obstacle.velocityX = -(6+score/100);
         obstacle.lifetime = 333;
         obstacle.shapeColor = 'red';
